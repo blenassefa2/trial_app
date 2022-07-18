1a31bc809599948b23b9e8f944a5d36a3424f5c0
Blen <62964622+blenassefa2@users.noreply.github.com>
2022-07-18 10:48:12 +0300

    second commit

--- static/script.js
+++
@@ -1,6 +0,0 @@
-const parallax=document.getElementById("div");
-
-window.addEventListener("scroll",function(){
-    let offset=window.pageYOffset;
-    parallax.style.backgroundPositionY=offset*(0.68) +"px";
-})
